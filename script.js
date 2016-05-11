51837655f1967bf66a1867786f6ac00ce902860e
katebolger <kate_bolger@greenwich.k12.ct.us>
2016-05-11 19:40:58 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Ms Bolger and the JavaScript Class. Thanks for checking us out.</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='Google1/index.html'>Google1</a></li>" +
+"<li><a href='Q4Project/index.html'>Q4Project</a></li>" +
+"</ul>";
